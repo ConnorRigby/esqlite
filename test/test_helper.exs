c269397a555cf6424f9fb84401a7f8542a286994
connor rigby <connor@farmbot.io>
2018-03-22 14:03:26 -0700

    Add a benchmark, update readme.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
 {:module, Esqlite3Nif} = :code.ensure_loaded(Esqlite3Nif)
 {:module, mod} = :code.ensure_loaded(:esqlite_test)
 :eunit.test({:inparallel, mod})
-ExUnit.start()
+ExUnit.start(exclude: :bench)
